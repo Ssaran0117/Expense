755010a791624fac7deb3678f6395ee1486dd77a
Ssaran0117 <92849629+Ssaran0117@users.noreply.github.com>
2021-10-21 11:12:05 +0530

    Create category.java

---
+++ model/category.java
@@ -0,0 +1,27 @@
+package Sai.Expense;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class category {
+	int category_id;
+	String name, description;
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
